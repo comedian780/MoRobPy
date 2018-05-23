f4955c89e4628fb6f7504598314e9291a98aed67
SabertoothX6 <sabertoothx6@github.com>
2018-05-23 11:08:52 +0200

    Added Textfile for loading\nAdded Reader Class for reading a texfile

--- main.py
+++ main.py
@@ -14,14 +14,36 @@ Created on Tue May 22 18:25:45 2018
              "+---+---+---+---+---+",
              "|   |   |   |   |   |",
              "+---+---+---+---+---+"]"""
+import json
+
+class reader:
+    def __init__(self):
+        file_content  = open("grid4x3.json").read()
+        self.data = json.loads(file_content)
+        x=self.data["width"]
+        y=self.data["height"]
+        cost=self.data["cost"]
+        blocks=[]
+        traps=[]
+        goals=[]
+        print(x,y,cost,self.data["block"]["0"]["x"])
+        for b in self.data["block"]:
+            blocks.append([self.data["block"][b]["x"],self.data["block"][b]["y"]])
+        for t in self.data["trap"]:
+            traps.append([self.data["trap"][t]["x"],self.data["trap"][t]["y"],self.data["trap"][t]["r"]])
+        for g in self.data["goal"]:
+            goals.append([self.data["trap"][g]["x"],self.data["trap"][g]["y"],self.data["trap"][g]["r"]])
+        self.world = gridworld(x,y,blocks,traps,goals,cost)
+        self.world.printWorld()
 
 class gridworld:
-    def __init__(self,_x,_y,_block,_trap,_goal):
+    def __init__(self,_x,_y,_block,_trap,_goal,_cost):
         self.x=_x
         self.y=_y
         self.block=_block
         self.trap =_trap
         self.goal =_goal
+        self.cost =_cost
         self.arr = []
         for ay in range(self.y):
             tmp = []
@@ -75,20 +97,31 @@ class gridworld:
                     print("     |",end="\n")
                 else:
                     print("     |",end="")
+                    
+    def printFieldArr(self,posY):
+        print("|",end="")
+        for elem in self.arr[posY]:
+            print(str(elem).ljust(5),end="")
+            print("|",end="")
+        print(end="\n")
         
     def printWorld(self):
+        i=0
         for h in range(self.y*2+1):
             if(h%2==1):
-                self.printField(h)
+                self.printFieldArr(i)
+                i+=1
             else:
                 self.printLine()
     
     def printArray(self):
         print(self.arr)
 
-world = gridworld(5,5,[[1,2],[2,2],[3,2]],[[3,1,-1]],[[3,0,1]])
+"""world = gridworld(5,5,[[1,2],[2,2],[3,2]],[[4,1,-10]],[[4,0,1]])
 world.printWorld()
-world.printArray()   
+world.printArray()"""
+read=reader()
+ 
     
 """for elem in gridworld:
     print(elem);"""
