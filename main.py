de2cec204cf769783222031e4263de0a1f862614
SabertoothX6 <sabertoothx6@github.com>
2018-05-24 19:51:30 +0200

    Fixed Bugs and finished Value Iteration Algorithm Steps are set manually

--- main.py
+++ main.py
@@ -37,18 +37,21 @@ class printer:
             print(str("").center(length,"-")+"+",end="")
         print(str("").center(length,"-")+"+")
         
-    def printField(self,arr,y,length):
+    def printField(self,arr,y,length,acc):
         print("|",end="")
         for elem in arr[y]:
-            print(str(elem).center(length),end="")
+            if(elem=='X'):
+                print(str(elem).center(length),end="")
+            else:
+                print(str(format(elem,".2f")).center(length),end="")
             print("|",end="")
         print(end="\n")
         
-    def printArray(self,array,length):
+    def printArray(self,array,length,acc):
         i=0
         for h in range(len(array)*2+1):
             if(h%2==1):
-                self.printField(array,i,length)
+                self.printField(array,i,length,acc)
                 i+=1
             else:
                 self.printLine(len(array[0]),length)
@@ -72,15 +75,16 @@ class gridworld:
         for ay in range(self.y):
             tmp = []
             for ax in range(self.x):
-                tmp.append(0)
+                tmp.append(0.0)
             self.arr.append(tmp)
         for b in self.block:
-            self.arr[b[1]][b[0]]="X"
+            self.arr[b[1]][b[0]]='X'
         for t in self.trap:
-            self.arr[t[1]][t[0]]=t[2]
+            self.arr[t[1]][t[0]]=float(t[2])
         for g in self.goal:
-            self.arr[g[1]][g[0]]=g[2]
+            self.arr[g[1]][g[0]]=float(g[2])
         print("Created ",self.x,"x",self.y,"Gridworld\n")
+        print("With",len(self.trap),"Traps and",len(self.goal),"Goals")
         
     """def printLine(self):
         print("+-----+",end="")
@@ -96,51 +100,74 @@ class gridworld:
         print(end="\n")"""
         
     def printWorld(self):
-        self.printer.printArray(self.arr,3)
+        self.printer.printArray(self.arr,4,1)
         
     def printState(self):
-        self.printer.printArray(self.rewArr,5)
+        self.printer.printArray(self.rewArr,5,2)
         
     def up(self,x,y):
-        if(y==0 or self.arr[y-1][x]=='X'):
+        if(y==0 or self.oldRewArr[y-1][x]=='X'):
             return self.oldRewArr[y][x]
         else:
             return self.oldRewArr[y-1][x]
         
     def left(self,x,y):
-        if(x==0 or self.arr[y][x-1]=='X'):
+        if(x==0 or self.oldRewArr[y][x-1]=='X'):
             return self.oldRewArr[y][x]
         else:
             return self.oldRewArr[y][x-1]
     
     def down(self,x,y):
-        if(y==(len(self.arr)-1) or self.arr[y+1][x]=='X'):
+        if(y==(len(self.oldRewArr)-1) or self.oldRewArr[y+1][x]=='X'):
             return self.oldRewArr[y][x]
         else:
             return self.oldRewArr[y+1][x]
             
     def right(self,x,y):
-        if(x==(len(self.arr)-1) or self.arr[y][x+1]=='X'):
+        if(x==(len(self.oldRewArr)-1) or self.oldRewArr[y][x+1]=='X'):
             return self.oldRewArr[y][x]
         else:
             return self.oldRewArr[y][x+1]
         
+    def isFieldTrap(self,x,y):
+        for i in range(0,len(self.trap)):
+            if(self.trap[i][0]==x and self.trap[i][1]):
+                return i
+        return -1
+    
+    def isFieldGoal(self,x,y):
+        for i in range(0,len(self.goal)):
+            if(self.goal[i][0]==x and self.goal[i][1]):
+                return i
+        return -1
+        
     def calcVIStep(self):
         self.oldRewArr = self.rewArr
         rew = []
-        for y in self.rewArr:
-            for x in self.rewArr[0]:
-                rew[0] = self.prob_succ*self.up(x,y)+self.prob_fail*self.left(x,y)+self.prob_fail*self.right(x,y)
-                rew[1] = self.prob_succ*self.left(x,y)+self.prob_fail*self.down(x,y)+self.prob_fail*self.up(x,y)
-                rew[2] = self.prob_succ*self.down(x,y)+self.prob_fail*self.right(x,y)+self.prob_fail*self.left(x,y)
-                rew[3] = self.prob_succ*self.right(x,y)+self.prob_fail*self.up(x,y)+self.prob_fail*self.down(x,y)
-                self.rewArr = self.cost + self.gamma+max(rew)
+        for y in range(0,len(self.oldRewArr)):
+            for x in range(0,len(self.oldRewArr[0])):
+                print("Position:",x,y,"Inhalt:",self.oldRewArr[y][x])
+                isTrap=self.isFieldTrap(x,y)
+                isGoal=self.isFieldGoal(x,y)
+                if(self.oldRewArr[y][x]=='X'):
+                    print("Block")
+                elif(isTrap>=0):
+                    print("")
+                elif(isGoal>=0):
+                    print("Goal")
+                else:
+                    rew.append(self.prob_succ*self.up(x,y)+self.prob_fail*self.left(x,y)+self.prob_fail*self.right(x,y))
+                    rew.append(self.prob_succ*self.left(x,y)+self.prob_fail*self.down(x,y)+self.prob_fail*self.up(x,y))
+                    rew.append(self.prob_succ*self.down(x,y)+self.prob_fail*self.right(x,y)+self.prob_fail*self.left(x,y))
+                    rew.append(self.prob_succ*self.right(x,y)+self.prob_fail*self.up(x,y)+self.prob_fail*self.down(x,y))
+                    self.rewArr[y][x] = self.cost + self.gamma * max(rew)
         
     def calcVI(self):
         notKonv = 1
-        while(notKonv):
+        while(notKonv<100):
             self.calcVIStep()
             self.printState()
+            notKonv+=1
             
         
     """def printWorld(self):
